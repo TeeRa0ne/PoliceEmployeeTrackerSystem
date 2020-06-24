a0f017ad9bd7658d27f873c1dad45907a876ecc1
TeeRa0ne <60188443+TeeRa0ne@users.noreply.github.com>
2020-06-24 11:50:34 +0200

    Add img

--- views/index.php
+++ views/index.php
@@ -6,6 +6,14 @@
     <title>Police Employee System</title>
 </head>
 <body>
+    <header>
+        <div class="header1">
+            <img src="~/assets/img/FBI.png" alt="logo">
+            <div>
+                <h1>Federal Bureau of Investigation</h1>
+            </div>
+        </div>
     
+    </header>
 </body>
 </html>
