a31600a3467940f94250b667d1abee65760ad57f
Théo LEQUIPE <lequipe.theo@hotmail.com>
2020-06-26 17:51:00 +0200

    Add new text + CSS

--- views/index.php
+++ views/index.php
@@ -4,26 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Police Employee System</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
-    <header>
+    <header class="global">
         <div class="header1">
-            <img src="../assets/img/FBI.png" alt="logo">
-            <div>
-                <h1 class="title">Federal Bureau of Investigation</h1>
-                <p>Authorized personal only</p>
-            </div>
+        <img class="logo" src="../assets/img/FBI.png" alt="logo">
+            <h1 class="title">Federal Bureau of Investigation</h1>
+            <p>Authorized personal only</p>
         </div>
     </header>
-    <div class="container">
-    <h2>Employee Police Database</h2>
-    <label for="login">Login :</label>
-    <input name="login" type="text">
-    <label for="pass">Password :</label>
-    <input name="pass" type="password">
-    
-    
+    <div class="background">
+        <div class="container">
+            <h2>Employee Police Database</h2>
+            <label class="login" for="login">Login :</label>
+            <input name="login" type="text">
+            <label for="pass">Password :</label>
+            <input name="pass" type="password">   
+            <div class="adminpanel-signin">
+                <a href="adminpanel.php">Admin Panel</a>
+                <button class="button-submit" type="submit">Submit</button>
+            </div>
+        </div>
     </div>
 </body>
 </html>
