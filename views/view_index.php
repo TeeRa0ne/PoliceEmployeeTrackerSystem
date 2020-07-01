80687ed33364e5d32bded41de874e97c199c6e58
TeeRa0ne <60188443+TeeRa0ne@users.noreply.github.com>
2020-07-01 09:49:49 +0200

    Modif Button

--- views/view_index.php
+++ views/view_index.php
@@ -6,7 +6,7 @@ if(isset($_POST['username']) && isset($_POST['pwd']))
         // connexion à la base de données
         $db_username = 'root';
         $db_password = '';
-        $db_name     = 'policeemploye';
+        $db_name     = 'policeemployee';
         $db_host     = 'localhost';
         $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
                or die('could not connect to database');
@@ -59,15 +59,21 @@ if(isset($_POST['username']) && isset($_POST['pwd']))
             <h2>Employee Police Database</h2>
             <form method="post">
                 <div class="form1">
-                    <label for="username">Username :</label>
-                    <input for="username" name="username" type="text">
-                    <br>
-                    <label for="pwd">Password :</label>
-                    <input for="pwd" name="pwd" type="password">
+                    <center>
+                        <div class="login">
+                            <label for="username">Username :</label>
+                            <input for="username" name="username" type="text">
+                        </div>
+                        <br>
+                        <div class="password">
+                            <label for="pwd">Password :</label>
+                            <input for="pwd" name="pwd" type="password">
+                        </div>
+                    </center>
                 </div>
             <div class="adminpanel-signin">
                 <a href="adminpanel.php">Admin Panel</a>
-                <button class="button-submit" type="submit" value="submit">Submit</button>
+                    <button class="button-submit" type="submit" value="submit">Submit</button>
             </div>
             <?php
                 if(isset($_GET['erreur'])){
