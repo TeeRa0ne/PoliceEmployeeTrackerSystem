e72e23104c5c69cd8ee9605aec204e6ce69eb138
TeeRa0ne <60188443+TeeRa0ne@users.noreply.github.com>
2020-07-07 11:39:33 +0200

    Edit adminpanel.css + add.php

--- views/adminfunction/add.php
+++ views/adminfunction/add.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Police Employee Tracker System</title>
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/adminpanel.css">
 </head>
 <body>
     <header class="global">
@@ -45,7 +45,7 @@
                 <label for="male">Male</label><br>
                     <input type="radio" id="female" name="gender" value="female">
                 <label for="female">Female</label><br>
-                        
+
             </form>
         </div>
     </div>
--- views/adminpanel.php
+++ views/adminpanel.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - Police Employee Tracker System</title>
-    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/adminpanel.css">
 </head>
 <body>
     <header class="global">
@@ -18,13 +18,13 @@
     </header>
     <hr>
     <div class="container">
-    <h2>Administration Panel</h2>
-    <ul class="list-com">
-        <li><a href="../views/adminfunction/add.php">Add new employee</a></li>
-        <li><a href="#">Edit existing employee</a></li>
-        <li><a href="#">Remove a employee</a></li>
-        <li><a href="#">List of employee</a></li>
-    </ul>
+        <h2>Administration Panel</h2>
+        <div class="button-list">
+            <button onclick=window.location.href='../views/adminfunction/add.php'; class="button-submit" type="submit"><a>Add new employee</a></button>
+            <button onclick=window.location.href='../views/adminfunction/edit.php'; class="button-submit" type="submit"><a href="#">Edit existing employee</a></button>
+            <button onclick=window.location.href='../views/adminfunction/remove.php'; class="button-submit" type="submit"><a href="#">Remove a employee</a></button>
+            <button onclick=window.location.href='../views/adminfunction/list.php'; class="button-submit" type="submit"><a href="#">List of employee</a></button>
+        </div>
     </div>
 </body>
 </html>
